77f085d72a1862907dc1fd9c2e0afbffe3e18da3
Alexandr-Zaburiannyi <ozaburiannyi@gmail.com>
2024-01-18 17:39:51 +0200

    third commit, fixed new animation

--- src/components/UI/CarCard.jsx
+++ src/components/UI/CarCard.jsx
@@ -1,11 +1,21 @@
+import { useRef } from "react";
 import { Link } from "react-router-dom";
 import classes from "./CarCard.module.scss";
 const CarCard = (props) => {
   const { index, onSelect, styles } = props;
+  const myRef = useRef(null);
+
+  const clickHandler = () => {
+    onSelect(index);
+    myRef.current.scrollIntoView({ behavior: "smooth", block: "center" });
+  };
+
   return (
     <div
-      onClick={() => onSelect(index)}
+      ref={myRef}
+      onClick={clickHandler}
       className={`${classes["car-container"]} ${styles}`}
+      id={index}
     >
       <img src={props.image} className={classes["car-image"]} alt="" />
       <div className={classes["link-container"]}>
